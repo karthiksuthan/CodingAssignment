678c4aabf55422da46077c9b3994e6e939cf09cd
Karthik Suthan <karthiksuthan@QB-IPL-2723.local>
2024-05-24 19:44:10 +0530

    Image assets added

--- src/assets/colors/index.ts
+++ src/assets/colors/index.ts
@@ -1,3 +1,5 @@
 export const ColorPallette = {
-    black:'#000',
+    black: '#000',
+    white: '#FFF',
+    headerBackground:'rgba(0,0,0,0.75)'
 }
---
+++ src/assets/images/index.ts
@@ -0,0 +1,15 @@
+export const BackIconImage = require('./Back.png');
+export const NavBar = require('./nav_bar.png');
+export const SearchIconImage = require('./search.png');
+export const PlaceholderPoster = require('./placeholder_for_missing_posters.png');
+export const Posters = {
+  'poster1.jpg': require('./poster1.jpg'),
+  'poster2.jpg': require('./poster2.jpg'),
+  'poster3.jpg': require('./poster3.jpg'),
+  'poster4.jpg': require('./poster4.jpg'),
+  'poster5.jpg': require('./poster5.jpg'),
+  'poster6.jpg': require('./poster6.jpg'),
+  'poster7.jpg': require('./poster7.jpg'),
+  'poster8.jpg': require('./poster8.jpg'),
+  'poster9.jpg': require('./poster9.jpg'),
+};
