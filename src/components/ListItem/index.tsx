96292c0c516d96a9152728084e39f0552293d0bc
Karthik Suthan <karthiksuthan@QB-IPL-2723.local>
2024-05-26 23:16:01 +0530

    long name handling

--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,24 +1,40 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React, { useState } from 'react'
-import { PlaceholderPoster, Posters } from '../../assets/images';
-import { styles } from './styles';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {PlaceholderPoster, Posters} from '../../assets/images';
+import {styles} from './styles';
+import { movieListItemType } from '../../types';
 
-const ListItem = ({ index, item, separators }) => {
-    const [isLongText, setLongText] = useState<boolean>(false);
-    const checkTextLength = (e) => {
-        if (e.nativeEvent.lines.length > 1) {
-            setLongText(true);
-       }
-    }
-    return(
-  <View style={styles.container}>
-    <Image
-      source={Posters?.[item?.['poster-image']] ?? PlaceholderPoster}
-      style={styles.posterImage}
-    />
-            { <Text onTextLayout={checkTextLength} style={styles.name} numberOfLines={1}>{item?.name}</Text>
-     }
-  </View>
-)};
+const ListItem = ({index, item}:{index:number,item:movieListItemType}) => {
+  const [isLongText, setLongText] = useState<boolean>(false);
+  const [toggle, setToggle] = useState<boolean>(false);
+  const checkTextLength = (e:any) => {
+    if (e.nativeEvent.lines.length > 1) {
+      setLongText(true);
+    } else setLongText(false);
+  };
+  const handlePress = () => {
+    if (isLongText)
+      setToggle(old=>!old)
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgWrap}>
+        <Image
+          source={Posters?.[item?.['poster-image']] ?? PlaceholderPoster}
+          style={styles.posterImage}
+        />
+      </View>
+      {
+        <Text
+          onPress={handlePress}
+          onTextLayout={checkTextLength}
+          style={styles.name}
+          numberOfLines={toggle ? 0 : 1}>
+          {item?.name}
+        </Text>
+      }
+    </View>
+  );
+};
 
-export default ListItem
+export default ListItem;
